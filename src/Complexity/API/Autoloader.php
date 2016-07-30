37a78c163b18f95144fdd2e0d9dbb0d491accb92
Roemer Bakker <contact@roemerbakker.com>
2016-07-30 14:22:42 +0200

    Wrote part of autoloader + main client class. Also started with objects.

---
+++ src/Complexity/API/Autoloader.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @license         Berkeley Software Distribution License (BSD-License 2) http://www.opensource.org/licenses/bsd-license.php
+ * @author          Roemer Bakker
+ * @copyright       Complexity Software
+ */
+
+namespace DimeboxApiPhp;
+
+/**
+ * PSR-0 Autoloader for Composer
+ *
+ * Class Dimebox_Api_Autoloader
+ * @package DimeboxApiHttpPhp
+ */
+class Dimebox_Api_Autoloader
+{
+    /**
+     * Finds all classes and requires them
+     *
+     * @param string $class_name
+     */
+    public static function autoload ($class_name)
+    {
+        if (strpos($class_name, "Dimebox_") === 0)
+        {
+            $file_name = str_replace("_", "/", $class_name);
+            $file_name = realpath(dirname(__FILE__) . "/../../{$file_name}.php");
+            if ($file_name !== false)
+            {
+                require_once($file_name);
+            }
+        }
+    }
+
+    /**
+     * Register all classes
+     *
+     * @return bool
+     */
+    public static function register ()
+    {
+        return spl_autoload_register(array(__CLASS__, "autoload"));
+    }
+
+    /**
+     * Unregister all classes
+     *
+     * @return bool
+     */
+    public static function unregister ()
+    {
+        return spl_autoload_unregister(array(__CLASS__, "autoload"));
+    }
+}
---
+++ src/Complexity/API/Client.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license         Berkeley Software Distribution License (BSD-License 2) http://www.opensource.org/licenses/bsd-license.php
+ * @author          Roemer Bakker
+ * @copyright       Complexity Software
+ */
+
+namespace DimeboxApiPhp;
+
+class Dimebox_Api_Client
+{
+    const CLIENT_VERSION = "1.0";
+
+    const API_ENDPOINT = "https://spryng.dimebox.com";
+}
