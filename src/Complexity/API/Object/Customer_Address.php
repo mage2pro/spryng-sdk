2b426802287bf8a2acd57675abd620242e499a39
Roemer Bakker <contact@roemerbakker.com>
2016-07-30 17:49:05 +0200

    Customer & customer address objects

---
+++ src/Complexity/API/Object/Customer_Address.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @license         Berkeley Software Distribution License (BSD-License 2) http://www.opensource.org/licenses/bsd-license.php
+ * @author          Roemer Bakker
+ * @copyright       Complexity Software
+ */
+
+namespace DimeboxApiPhp\Object;
+
+/**
+ * Class Dimebox_Api_Object_Customer_Address
+ * @package DimeboxApiPhp\Object
+ */
+class Dimebox_Api_Object_Customer_Address
+{
+    /**
+     * A customer can have many address objects. e.g. Shipping, Billing, Office or Home.
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * Street address of the customer.
+     *
+     * @var string
+     */
+    public $street_address;
+
+    /**
+     * Region of the customer e.g. state or province.
+     *
+     * @var string
+     */
+    public $region;
+
+    /**
+     * Locality of the customer e.g. city or town.
+     *
+     * @var string
+     */
+    public $locality;
+
+    /**
+     * Postal code of the customer.
+     *
+     * @var string
+     */
+    public $postal_code;
+
+    /**
+     * Available for additional address details.
+     *
+     * @var string
+     */
+    public $extended_address;
+
+    /**
+     * Two character ISO country code.
+     *
+     * @var string
+     */
+    public $country_code;
+}
