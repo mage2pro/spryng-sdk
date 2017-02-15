ce7eb924c7f23f7d3810266083bc78e8a926f3a8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-15 18:05:01 +0300

    1.1.5: https://github.com/spryngpayments/php/issues/2

--- src/Complexity/SpryngPaymentsApiPhp/Controller/AccountController.php
+++ src/Complexity/SpryngPaymentsApiPhp/Controller/AccountController.php
@@ -1,38 +1,43 @@
 <?php
-
 namespace SpryngPaymentsApiPhp\Controller;
-
-use SpryngPaymentsApiPhp\Helpers\AccountHelper;
-use SpryngPaymentsApiPhp\Client;
+use SpryngPaymentsApiPhp\Helpers\AccountHelper as H;
+use SpryngPaymentsApiPhp\Object\Account;
+use SpryngPaymentsApiPhp\SpryngPaymentsException as E;
 use SpryngPaymentsApiPhp\Utility\RequestHandler;
+class AccountController extends BaseController{
+	/**       
+	 * 2017-02-15
+	 * @return Account[]
+	 */
+    public function getAll() {return array_map(function(array $a) {return
+		H::fill($a)
+	;}, $this->res());}
 
-class AccountController extends BaseController
-{
-    const ACCOUNT_URI = "/account";
-
-    public function __construct(Client $api)
-    {
-        parent::__construct($api);
+    /**
+     * @param string $id
+     * @return Account
+     * @throws E
+     */
+    public function getById($id) {
+    	if (!($res = $this->res($id))) {
+			throw new E('Account not found', 202);
+		}
+    	return H::fill($res[0]);
     }
 
-    public function getAll()
-    {
-        $http = new RequestHandler();
-        $http->setHttpMethod("GET");
-        $http->setBaseUrl($this->api->getApiEndpoint());
-        $http->setQueryString(static::ACCOUNT_URI);
-        $http->addHeader($this->api->getApiKey(), 'X-APIKEY');
-        $http->doRequest();
-
-        $response = json_decode($http->getResponse());
-        $accounts = array();
-
-        foreach($response as $account)
-        {
-            $accountObj = AccountHelper::fill($account);
-            array_push($accounts, $accountObj);
-        }
-
-        return $accounts;
-    }
+	/**
+	 * 2017-02-15
+	 * @param string|null $id [optional]
+	 * @return array(array(string => mixed))
+	 */
+    private function res($id = null) {
+    	/** @var RequestHandler $req */
+        $req = new RequestHandler;
+        $req->setHttpMethod('GET');
+        $req->setBaseUrl($this->api->getApiEndpoint());
+        $req->setQueryString('/account' . (!$id ? '' : "?_id=$id"));
+        $req->addHeader($this->api->getApiKey(), 'X-APIKEY');
+        $req->doRequest();
+        return json_decode($req->getResponse());
+	}
 }
